a7d28b2483a97606b997dceaf2ce27bc18d0faed
packtty18 <packtty18@naver.com>
2025-05-28 20:58:04 +0900

    2025-5-28
    1. 초기 기본폴더 추가
    2. 메인 매니저 구상 및 코딩(완성되지 않음)

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers _instance;
+
+    public static Managers Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("Managers가 Init()되지 않았습니다.");
+            }
+            return _instance;
+        }
+    }
+
+    public static void Init()
+    {
+        if (_instance != null)
+            return;
+
+        Debug.Log("Managers 초기화 시작");
+        GameObject managerObj = GameObject.Find("Managers");
+        if (managerObj == null)
+        {
+            managerObj = new GameObject("Managers");
+            _instance = managerObj.AddComponent<Managers>();
+        }
+        else
+        {
+            _instance = managerObj.GetComponent<Managers>();
+            if (_instance == null)
+            {
+                _instance = managerObj.AddComponent<Managers>();
+            }
+        }
+
+        DontDestroyOnLoad(managerObj);
+        _instance.InitManagers();
+        Debug.Log("Managers 초기화 완료");
+    }
+
+    public void Clear()
+    {
+        ClearManagers();
+    }
+
+    private void InitManagers()
+    {
+        Debug.Log("하위 매니저 초기화 시작");
+        // 하위 매니저 초기화 로직들
+        Debug.Log("하위 매니저 초기화 완료");
+    }
+
+    public void ClearManagers()
+    {
+        Debug.Log("하위 매니저 종료 시작");
+        // 하위 매니저 정리 로직들
+        Debug.Log("하위 매니저 종료 완료");
+    }
+}
